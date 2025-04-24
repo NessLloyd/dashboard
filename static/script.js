3e77a62638220e363c4822022d5692a7c44a9b64
Vanessa Lloyd <165439848+NessLloyd@users.noreply.github.com>
2025-04-24 17:10:58 +1000

    Update script.js

--- static/script.js
+++ static/script.js
@@ -21,35 +21,35 @@ teamLinks.forEach(link => {
         break;
 
       case "chi":
-        embedContainer.innerHTML = `<p>Chi’s project is not embedded yet. Coming soon!</p>`;
+
         break;
 
       case "daryl":
-        embedContainer.innerHTML = `<p>Daryl’s project is not embedded yet. Coming soon!</p>`;
+
         break;
 
       case "zach":
-        embedContainer.innerHTML = `<p>Zach’s project is not embedded yet. Coming soon!</p>`;
+ 
         break;
 
       case "vanessa":
-        embedContainer.innerHTML = `<p>Vanessa’s project is not embedded yet. Coming soon!</p>`;
+      
         break;
 
       case "simon":
-        embedContainer.innerHTML = `<p>Simon’s project is not embedded yet. Coming soon!</p>`;
+    
         break;
 
       case "joshua":
-        embedContainer.innerHTML = `<p>Joshua and Vireak’s project is not embedded yet. Coming soon!</p>`;
+   
         break;
 
       case "leon":
-        embedContainer.innerHTML = `<p>Leon’s project is not embedded yet. Coming soon!</p>`;
+
         break;
 
       default:
-        embedContainer.innerHTML = `<p>No content available for this user.</p>`;
+
     }
   });
 });
