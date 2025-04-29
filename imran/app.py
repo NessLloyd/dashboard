6e9f30fe3435987a4ffa955f10cb180e318b9c44
Vanessa Lloyd <165439848+NessLloyd@users.noreply.github.com>
2025-04-29 11:14:48 +1000

    Update app.py

--- imran/app.py
+++ imran/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from analyzer import EnhancedMisDisInfoDetector
+from analyzer import MisDisInfoDetector
 from datetime import datetime, timedelta
 import json
 
